807c93520fea94eb3a5b043cd426d1064376ed95
Brad-Duderstadt <brad.duderstadt@coxautoinc.com>
2021-08-14 16:29:44 -0500

    Added transpiling, bundling, linting and testing

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,23 @@
-var express = require('express');
-var path = require('path');
-var open = require('open');
+/*
+  You can serve this with localtunnel
+  lt --port <port>
+  lt --port <port> --subdomain <name>
+*/
+import express from 'express';
+import path from 'path';
+import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
+
+/* eslint-disable no-console */
 
 const port = 3000;
 const app = express();
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  publicPath: config.output.publicPath
+}));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../src/index.html'));
